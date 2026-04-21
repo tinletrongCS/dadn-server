e966e12929149220b8061fdfa223cd2ef657a226
lttinCS <162774530+tinletrongCSE@users.noreply.github.com>
2026-04-21 11:15:48 +0700

    logout not donw

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -2,8 +2,8 @@ from fastapi import APIRouter, Depends, status
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from database import get_db
-from schemas.domain_schemas import TokenResponse, UserResponse,UserResponseMe, UserCreate
-from services import auth_service
+from schemas.domain_schemas import TokenResponse, UserResponse, UserResponseMe, UserCreate, MessageResponse
+from services import auth_service, log_service
 from dependencies.auth_deps import get_current_user
 from models.domain_models import User
 
@@ -29,3 +29,12 @@ async def login(
 @router.get("/me", response_model=UserResponseMe)
 async def read_current_user(current_user: User = Depends(get_current_user)):
     return current_user
+
+
+# Đăng xuất 
+@router.post("/logout", response_model=MessageResponse)
+async def logout(
+    current_user: User = Depends(get_current_user),
+    db: Session = Depends(get_db)
+):
+    return await auth_service.logout(db, current_user)
